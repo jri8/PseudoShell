f4fc9a2d901fbabeab137ead2bebea5fb1e3d6d4
osboxes.org <Jackson>
2018-01-30 23:39:46 +0000

    tokenized input will output to screen

--- myshell.c
+++ myshell.c
@@ -1,6 +1,7 @@
 //main file for Pseudo Shell
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /*----------TO-DO-----------
 //tokenize input
 //figureout wtf to do
@@ -23,22 +24,38 @@ int main(void){
 	memset(buffer,0,sizeof(buffer));
 	char *history[100];
 	int histInd = 0;
+
+	char *token;
 	//use circular array to keep track of history
 	do{
+		//displaying prompt
 		printf("$");
+		
+		//reading input
 		fgets(buffer,2048, stdin);
-		history[histInd] = malloc(strlen(buffer)+1);
-		strcpy(history[histInd], buffer);
 
-	//	history[histInd] = buffer;
+		//allocating space in array for history
+		free(history[histInd]); 
+		history[histInd] = malloc(strlen(buffer)+1);
+		//copies buffer into history array
+		strcpy(history[histInd], buffer);
+		//maintaining index
 		histInd++;
 		histInd = histInd % 100;
+		
+		//tokenizing input
+		token = strtok(buffer, " ");	
+		while (token != NULL){
+			printf("%s\n", token);
+			token = strtok(NULL, " ");
+		}
 		
 	}while(strncmp(buffer, "exit", 4) != 0);
 	int i;
 	for(i = 0; i < histInd; i++){
 		printf("%d %s",i, history[i]);
 	}
+	
 	return 0;
 	
 }
