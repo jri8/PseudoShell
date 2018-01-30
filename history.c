b99d070e111f657d184573d8a53040c26c52868f
osboxes.org <Jackson>
2018-01-30 22:39:34 +0000

    history added to myshell.c, history file created but may not be necessary

---
+++ history.c
@@ -0,0 +1,9 @@
+//history file
+//with command history it should ouput history of command inputs
+//this history will be mainiatined in a txt file by the parent process
+//if -c modifier then clear history
+	//clear file (either in parent or in child process?)
+
+// if [offset] modifier
+
+//ask in class tomorrow
--- myshell.c
+++ myshell.c
@@ -1,19 +1,44 @@
 //main file for Pseudo Shell
 #include <stdio.h>
 #include <string.h>
+/*----------TO-DO-----------
+//tokenize input
+//figureout wtf to do
+//fork()
+//manage stdin/out/err
+//execve
+//make other files with code to execute given commands
+//(exit, cd, history)
+//error messages
+------------TO-DO----------*/
+
+
+
+
+
+
 int main(void){
 	
 	char buffer[2048];
 	memset(buffer,0,sizeof(buffer));
-
+	char *history[100];
+	int histInd = 0;
+	//use circular array to keep track of history
 	do{
 		printf("$");
 		fgets(buffer,2048, stdin);
-		printf("%s", buffer);
-		int x = strncmp(buffer, "exit", 4);
-		printf("%d\n", x);
-	}while(strncmp(buffer, "exit", 4) != 0);
+		history[histInd] = malloc(strlen(buffer)+1);
+		strcpy(history[histInd], buffer);
 
+	//	history[histInd] = buffer;
+		histInd++;
+		histInd = histInd % 100;
+		
+	}while(strncmp(buffer, "exit", 4) != 0);
+	int i;
+	for(i = 0; i < histInd; i++){
+		printf("%d %s",i, history[i]);
+	}
 	return 0;
 	
 }
